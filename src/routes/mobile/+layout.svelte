b47bbd14cda2159f178bc6e5dbdea932661ccb97
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2023-10-22 15:31:34 +0800

    [FIX] Navbar and others

--- src/routes/mobile/+layout.svelte
+++ src/routes/mobile/+layout.svelte
@@ -2,7 +2,7 @@
 	import MobileNavbar from "$lib/components/MobileNavbar.svelte"
   import { page } from "$app/stores";
 </script>
-<div class="w-screen- h-[90vh] overflow-hidden">
+<div class="w-screen { $page.url.pathname === '/mobile/splash' || $page.url.pathname === '/mobile/login' || $page.url.pathname === '/mobile/signup' ? 'h-screen' : 'h-[90vh]'} overflow-hidden">
   <slot />
 </div>
 {#if $page.url.pathname !== '/mobile/splash' && $page.url.pathname !== '/mobile/login' && $page.url.pathname !== '/mobile/signup'}
