bb26df11345e985b4886aa5e82a1f8f23473057c
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2023-11-25 18:00:26 +0800

    [FIX] Login enter key

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -75,7 +75,7 @@
   <div class="col-span-8 bg-success bg-plants bg-no-repeat bg-cover" />
   <div class="col-span-4 flex justify-center items-center p-6">
 
-    <form>
+    <form on:submit={() => {return false}}>
       <div>
         <div class="flex justify-center items-center w-full bg-yellow">
           <img src="/ezgif.png" alt="logo">
@@ -90,9 +90,55 @@
       </div>
 
       <div class="flex flex-col gap-2">
-        <MacroInput bind:value={creds.email} name="Email" icon='Person' placeholder="user@host.domain" />
-        <MacroPassword bind:value={creds.password} name="Password" icon='Key' placeholder="*******" />
-        <button on:click={() => login()} class="btn btn-primary btn-block text-white poppins text-lg mt-10 ">
+        <!-- <MacroInput bind:value={creds.email} name="Email" icon='Person' placeholder="user@host.domain" /> -->
+        <div class='w-full'>
+          <label for="Email" class="label poppins font-medium">
+            Email <span class="text-error ml-2 grow">*</span>
+          </label>
+          
+          <div class="w-full rounded-xl border-primary border-2 flex items-center">
+            <div class="p-2 centerxy">
+              <span class="material-symbols-rounded text-primary">
+                person
+              </span>
+            </div>
+
+            <input
+              id='Email'
+              class="bg-transparent form-input input md:input-lg w-full border-none poppins text-base placeholder:text-primary pl-0"
+              bind:value={creds.email}
+              required
+              placeholder="user@host.domain"
+              name="Email"
+              type='text'
+            />
+          </div>
+        </div>
+        <div class='w-full'>
+          <label for="Pass" class="label poppins font-medium">
+            Password <span class="text-error ml-2 grow">*</span>
+          </label>
+          
+          <div class="w-full rounded-xl border-primary border-2 flex items-center">
+            <div class="p-2 centerxy">
+              <span class="material-symbols-rounded text-primary">
+                key
+              </span>
+            </div>
+
+            <input
+              id='Pass'
+              class="bg-transparent form-input input md:input-lg w-full border-none poppins text-base placeholder:text-primary pl-0"
+              bind:value={creds.password}
+              required
+              placeholder="Password"
+              name="Email"
+              type='password'
+            />
+          </div>
+        </div>
+        <!-- <MacroPassword bind:value={creds.password} name="Password" icon='Key' placeholder="*******" /> -->
+        <button type="button" on:click={() => login()} class="btn btn-primary btn-block text-white poppins text-lg mt-10 ">
           {#if !loggingin}
             <div in:fade={{ duration: $pageTransitionDuration, delay: $pageTransitionDuration }} out:fade={{ duration: $pageTransitionDuration }}>
               Login
